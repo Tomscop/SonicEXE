9d1ac649400e95b7d21ad95f216f227bc2d0463f
Lucky <baloghmarcellkrisztofer@gmail.com>
2022-08-03 19:12:45 +0200

    You Cant Run Development

--- src/characters/ycn/ycn.c
+++ src/characters/ycn/ycn.c
@@ -12,14 +12,43 @@ enum
 	YCN_ArcMain_Idle1,
 	YCN_ArcMain_Left0,
 	YCN_ArcMain_Left1,
+	YCN_ArcMain_Left2,
 	YCN_ArcMain_Down0,
 	YCN_ArcMain_Down1,
+	YCN_ArcMain_Down2,
 	YCN_ArcMain_Up0,
 	YCN_ArcMain_Up1,
+	YCN_ArcMain_Up2,
 	YCN_ArcMain_Right0,
 	YCN_ArcMain_Right1,
+	YCN_ArcMain_Right2,
+	YCN_ArcMain_Pixel,
+	YCN_ArcMain_MadIdle0,
+	YCN_ArcMain_MadIdle1,
+	YCN_ArcMain_MadLeft0,
+	YCN_ArcMain_MadLeft2,
+	YCN_ArcMain_MadDown0,
+	YCN_ArcMain_MadDown2,
+	YCN_ArcMain_MadUp0,
+	YCN_ArcMain_MadUp2,
+	YCN_ArcMain_MadRight0,
+	YCN_ArcMain_MadRight2,
 	YCN_ArcMain_Laugh0,
 	YCN_ArcMain_Laugh1,
+	YCN_ArcMain_Laugh2,
+	YCN_ArcMain_Scream0,
+	YCN_ArcMain_Scream1,
+	YCN_ArcMain_Scream2,
+	YCN_ArcMain_Scream3,
+	YCN_ArcMain_Scream4,
+	YCN_ArcMain_Scream5,
+	YCN_ArcMain_JustDie0,
+	YCN_ArcMain_JustDie1,
+	YCN_ArcMain_JustDie2,
+	YCN_ArcMain_JustDie3,
+	YCN_ArcMain_JustDie4,
+	YCN_ArcMain_JustDie5,
+	YCN_ArcMain_JustDie6,
 	
 	YCN_Arc_Max,
 };
@@ -39,47 +68,41 @@ typedef struct
 
 //YCN character definitions
 static const CharFrame char_ycn_frame[] = {
-	{YCN_ArcMain_Idle0, {  0,   0, 106, 165}, { 53, 165}}, //0
-	{YCN_ArcMain_Idle0, {107,   0, 106, 165}, { 53, 165}}, //1
-	{YCN_ArcMain_Idle1, {  0,   0, 105, 163}, { 52, 165}}, //2
-	{YCN_ArcMain_Idle1, {106,   0, 105, 163}, { 52, 165}}, //3
-	{YCN_ArcMain_Idle1, {  0,   0, 106, 164}, { 53, 165}}, //4
+	{YCN_ArcMain_Idle0, {  0,   0, 127, 184}, { 55, 175}}, //0
+	{YCN_ArcMain_Idle0, {128,   0, 126, 184}, { 55, 175}}, //1
+	{YCN_ArcMain_Idle1, {  0,   0, 126, 184}, { 55, 175}}, //2
+	{YCN_ArcMain_Idle1, {127,   0, 127, 184}, { 55, 175}}, //3
 	
-	{YCN_ArcMain_Left0, {  0,   0,  130, 170}, { 80, 170}}, //4
-	{YCN_ArcMain_Left0, {131,   6,  121, 164}, { 70, 164}}, //5
-	{YCN_ArcMain_Left1, {  0,   0,  122, 163}, { 70, 163}}, //4
-	{YCN_ArcMain_Left1, {123,   1,  122, 162}, { 70, 162}}, //5
+	{YCN_ArcMain_Left0, {  0,   0,  168, 170}, { 110, 161}}, //4
+	{YCN_ArcMain_Left1, {  0,   0,  168, 170}, { 102, 162}}, //5
+	{YCN_ArcMain_Left2, {  0,   0,  172, 171}, { 105, 163}}, //6
 	
-	{YCN_ArcMain_Down0, {  0,   0, 129, 133}, { 59, 133}}, //6
-	{YCN_ArcMain_Down0, {  0,   0, 137, 142}, { 67, 141}}, //7
-	{YCN_ArcMain_Down1, {  0,   0, 138, 144}, { 67, 143}}, //6
-	{YCN_ArcMain_Down1, {  0,   0, 137, 144}, { 67, 143}}, //7
+	{YCN_ArcMain_Down0, {  0,   0, 142, 155}, { 74, 153}}, //7
+	{YCN_ArcMain_Down1, {  0,   0, 145, 159}, { 73, 157}}, //8
+	{YCN_ArcMain_Down2, {  0,   0, 141, 158}, { 72, 156}}, //9
 	
-	{YCN_ArcMain_Up0, {  0,   0, 150, 191}, { 71, 191}}, //8
-	{YCN_ArcMain_Up0, {  0,   0, 152, 183}, { 72, 183}}, //9
-	{YCN_ArcMain_Up1, {  0,   0, 154, 182}, { 73, 182}}, //8
-	{YCN_ArcMain_Up1, {  0,   0, 153, 182}, { 74, 182}}, //9
+	{YCN_ArcMain_Up0, {  0,   0, 127, 192}, { 60, 192}}, //10
+	{YCN_ArcMain_Up1, {  0,   0, 132, 191}, { 62, 191}}, //11
+	{YCN_ArcMain_Up2, {  0,   0, 132, 185}, { 62, 185}}, //12
 	
-	{YCN_ArcMain_Right0, {  4,   0, 127, 177}, { 43, 177}}, //10
-	{YCN_ArcMain_Right0, {132,   0, 116, 177}, { 42, 177}}, //11
-	{YCN_ArcMain_Right1, {  0,   0, 116, 179}, { 43, 179}}, //10
-	{YCN_ArcMain_Right1, {117,   0, 115, 179}, { 42, 179}}, //11
+	{YCN_ArcMain_Right0, {  0,   0, 179, 153}, { 37, 149}}, //13
+	{YCN_ArcMain_Right1, {  0,   0, 160, 154}, { 40, 150}}, //14
+	{YCN_ArcMain_Right2, {  0,   0, 170, 154}, { 39, 150}}, //15
 	
-	{YCN_ArcMain_Laugh0, {  0,   0, 117, 199}, { 43, 189}}, //10
-	{YCN_ArcMain_Laugh0, {118,   0, 114, 199}, { 42, 189}}, //11
-	{YCN_ArcMain_Laugh1, {  0,   0, 117, 199}, { 43, 189}}, //10
-	{YCN_ArcMain_Laugh1, {118,   0, 114, 199}, { 42, 189}}, //11
+	{YCN_ArcMain_Right0, {  0,   0, 117, 199}, { 43, 189}}, //16
+	{YCN_ArcMain_Right1, {  0,   0, 114, 199}, { 42, 189}}, //17
+	{YCN_ArcMain_Right2, {  0,   0, 117, 199}, { 43, 189}}, //18
 };
 
 static const Animation char_ycn_anim[CharAnim_Max] = {
-	{2, (const u8[]){ 0,  1,  2,  3,  4, ASCR_CHGANI, CharAnim_Idle}}, //CharAnim_Idle
-	{2, (const u8[]){ 5,  6,  7,  8, ASCR_BACK}},         //CharAnim_Left
+	{2, (const u8[]){ 0,  1,  2,  3, ASCR_CHGANI, CharAnim_Idle}}, //CharAnim_Idle
+	{2, (const u8[]){ 4,  5,  6, ASCR_BACK}},         //CharAnim_Left
 	{0, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},   //CharAnim_LeftAlt
-	{2, (const u8[]){ 9,  10, 11, 12, ASCR_BACK}},         //CharAnim_Down
-	{2, (const u8[]){ 21,  22, 23, 24, ASCR_BACK}},         //CharAnim_DownAlt
-	{2, (const u8[]){ 13,  14, 15, 16, ASCR_BACK}},         //CharAnim_Up
+	{2, (const u8[]){ 7,  8, 9, ASCR_BACK}},         //CharAnim_Down
+	{2, (const u8[]){ 16,  17, 18, ASCR_BACK}},         //CharAnim_DownAlt
+	{2, (const u8[]){ 10,  11, 12, ASCR_BACK}},         //CharAnim_Up
 	{0, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},   //CharAnim_UpAlt
-	{2, (const u8[]){17, 18, 19, 20,ASCR_BACK}},         //CharAnim_Right
+	{2, (const u8[]){13, 14, 15, ASCR_BACK}},         //CharAnim_Right
 	{0, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},   //CharAnim_RightAlt
 };
 
@@ -164,14 +187,43 @@ Character *Char_YCN_New(fixed_t x, fixed_t y)
 		"idle1.tim", //YCN_ArcMain_Idle1
 		"left0.tim",  //YCN_ArcMain_Left0
 		"left1.tim",  //YCN_ArcMain_Left1
+		"left2.tim",  //YCN_ArcMain_Left1
 		"down0.tim",  //YCN_ArcMain_Down0
 		"down1.tim",  //YCN_ArcMain_Down1
+		"down2.tim",  //YCN_ArcMain_Down1
 		"up0.tim",    //YCN_ArcMain_Up0
 		"up1.tim",    //YCN_ArcMain_Up1
+		"up2.tim",    //YCN_ArcMain_Up1
 		"right0.tim", //YCN_ArcMain_Right0
 		"right1.tim", //YCN_ArcMain_Right1
+		"right2.tim", //YCN_ArcMain_Right1
+		"pixel.tim", //YCN_ArcMain_Right1
+		"midle0.tim", //YCN_ArcMain_Idle0
+		"midle1.tim", //YCN_ArcMain_Idle1
+		"mleft0.tim",  //YCN_ArcMain_Left0
+		"mleft2.tim",  //YCN_ArcMain_Left1
+		"mdown0.tim",  //YCN_ArcMain_Down0
+		"mdown2.tim",  //YCN_ArcMain_Down1
+		"mup0.tim",    //YCN_ArcMain_Up0
+		"mup2.tim",    //YCN_ArcMain_Up1
+		"mright0.tim", //YCN_ArcMain_Right0
+		"mright2.tim", //YCN_ArcMain_Right1
 		"laugh0.tim", //YCN_ArcMain_Laugh0
 		"laugh1.tim", //YCN_ArcMain_Laugh1
+		"laugh2.tim", //YCN_ArcMain_Laugh1
+		"scream0.tim", //YCN_ArcMain_Laugh0
+		"scream1.tim", //YCN_ArcMain_Laugh1
+		"scream2.tim", //YCN_ArcMain_Laugh1
+		"scream3.tim", //YCN_ArcMain_Laugh0
+		"scream4.tim", //YCN_ArcMain_Laugh1
+		"scream5.tim", //YCN_ArcMain_Laugh1
+		"justdie0.tim", //YCN_ArcMain_Laugh0
+		"justdie1.tim", //YCN_ArcMain_Laugh1
+		"justdie2.tim", //YCN_ArcMain_Laugh1
+		"justdie3.tim", //YCN_ArcMain_Laugh0
+		"justdie4.tim", //YCN_ArcMain_Laugh1
+		"justdie5.tim", //YCN_ArcMain_Laugh1
+		"justdie6.tim", //YCN_ArcMain_Laugh1
 		NULL
 	};
 	IO_Data *arc_ptr = this->arc_ptr;
