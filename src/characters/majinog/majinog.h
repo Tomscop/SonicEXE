000c12436340fc3a0c1bd4b2dc22abcc0a524b95
Pichuman10 <107434295+Pichuman100@users.noreply.github.com>
2022-10-19 09:19:20 -0400

    majinog.h

---
+++ src/characters/majinog/majinog.h
@@ -0,0 +1,9 @@
+#ifndef PSXF_GUARD_MAJINOG_H
+#define PSXF_GUARD_MAJINOG_H
+
+#include "../../stage/character.h"
+
+//Majin OG character functions
+Character *Char_MajinOG_New(fixed_t x, fixed_t y);
+
+#endif
