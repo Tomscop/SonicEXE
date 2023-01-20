5329a14c164d8d987e5a5f4e63ce2c5f2f26d350
g3yt <83846251+g3yt@users.noreply.github.com>
2023-01-20 11:37:38 +0000

    xenophanes (not finished yet)

---
+++ iso/characters/xenophanes/dad.h
@@ -0,0 +1,83 @@
+iso/characters/dad/main.arc: iso/characters/dad/dad0.tim iso/characters/dad/dad1.tim iso/characters/dad/dad2.tim iso/characters/dad/dad3.tim iso/characters/dad/dad4.tim iso/characters/dad/dad5.tim iso/characters/dad/dad6.tim iso/characters/dad/dad7.tim iso/characters/dad/dad8.tim iso/characters/dad/dad9.tim iso/characters/dad/dad10.tim iso/characters/dad/dad11.tim iso/characters/dad/dad12.tim iso/characters/dad/dad13.tim iso/characters/dad/dad14.tim iso/characters/dad/dad15.tim iso/characters/dad/dad16.tim iso/characters/dad/dad17.tim iso/characters/dad/dad18.tim iso/characters/dad/dad19.tim iso/characters/dad/dad20.tim iso/characters/dad/dad21.tim iso/characters/dad/dad22.tim iso/characters/dad/dad23.tim 
+
+  "dad0.tim",
+  "dad1.tim",
+  "dad2.tim",
+  "dad3.tim",
+  "dad4.tim",
+  "dad5.tim",
+  "dad6.tim",
+  "dad7.tim",
+  "dad8.tim",
+  "dad9.tim",
+  "dad10.tim",
+  "dad11.tim",
+  "dad12.tim",
+  "dad13.tim",
+  "dad14.tim",
+  "dad15.tim",
+  "dad16.tim",
+  "dad17.tim",
+  "dad18.tim",
+  "dad19.tim",
+  "dad20.tim",
+  "dad21.tim",
+  "dad22.tim",
+  "dad23.tim",
+
+
+  Dad_ArcMain_Dad0,
+  Dad_ArcMain_Dad1,
+  Dad_ArcMain_Dad2,
+  Dad_ArcMain_Dad3,
+  Dad_ArcMain_Dad4,
+  Dad_ArcMain_Dad5,
+  Dad_ArcMain_Dad6,
+  Dad_ArcMain_Dad7,
+  Dad_ArcMain_Dad8,
+  Dad_ArcMain_Dad9,
+  Dad_ArcMain_Dad10,
+  Dad_ArcMain_Dad11,
+  Dad_ArcMain_Dad12,
+  Dad_ArcMain_Dad13,
+  Dad_ArcMain_Dad14,
+  Dad_ArcMain_Dad15,
+  Dad_ArcMain_Dad16,
+  Dad_ArcMain_Dad17,
+  Dad_ArcMain_Dad18,
+  Dad_ArcMain_Dad19,
+  Dad_ArcMain_Dad20,
+  Dad_ArcMain_Dad21,
+  Dad_ArcMain_Dad22,
+  Dad_ArcMain_Dad23,
+
+
+
+  {Dad_ArcMain_Dad0, {  2,  0,129,217}, { 86, 84}}, //0 idle 1
+  {Dad_ArcMain_Dad1, {  0,  0,128,216}, { 86, 82}}, //1 idle 2
+  {Dad_ArcMain_Dad2, {  0,  0,127,217}, { 86, 83}}, //2 idle 3
+  {Dad_ArcMain_Dad3, {  0,  0,127,218}, { 87, 84}}, //3 idle 4
+  {Dad_ArcMain_Dad4, {  0,  0,130,219}, { 90, 85}}, //4 idle 5
+  {Dad_ArcMain_Dad5, {  0,  0,128,223}, { 89, 89}}, //5 idle 6
+  {Dad_ArcMain_Dad6, {  0,  0,128,223}, { 89, 89}}, //6 idle 7
+  {Dad_ArcMain_Dad7, {  0,  0,128,223}, { 89, 89}}, //7 idle 8
+
+  {Dad_ArcMain_Dad8, {  0,  0,206,203}, {129, 68}}, //8 left 1
+  {Dad_ArcMain_Dad9, {  0,  0,202,203}, {123, 68}}, //9 left 2
+  {Dad_ArcMain_Dad10, {  0,  0,194,197}, {105, 62}}, //10 left 3
+  {Dad_ArcMain_Dad11, {  0,  0,199,199}, {110, 64}}, //11 left 4
+
+  {Dad_ArcMain_Dad12, {  0,  0,146,191}, { 95, 57}}, //12 down 1
+  {Dad_ArcMain_Dad13, {  0,  0,146,203}, { 95, 68}}, //13 down 2
+  {Dad_ArcMain_Dad14, {  0,  0,146,199}, { 95, 65}}, //14 down 3
+  {Dad_ArcMain_Dad15, {  0,  0,145,200}, { 94, 65}}, //15 down 4
+
+  {Dad_ArcMain_Dad16, {  0,  0,149,242}, {100,105}}, //16 up 1
+  {Dad_ArcMain_Dad17, {  0,  0,149,239}, {100,102}}, //17 up 2
+  {Dad_ArcMain_Dad18, {  0,  0,149,228}, {100, 92}}, //18 up 3
+  {Dad_ArcMain_Dad19, {  0,  0,149,230}, {100, 94}}, //19 up 4
+
+  {Dad_ArcMain_Dad20, {  0,  0,179,207}, { 44, 74}}, //20 right 1
+  {Dad_ArcMain_Dad21, {  0,  0,178,208}, { 44, 75}}, //21 right 2
+  {Dad_ArcMain_Dad22, {  0,  0,174,207}, { 44, 74}}, //22 right 3
+  {Dad_ArcMain_Dad23, {  0,  0,174,208}, { 44, 75}}, //23 right 4
