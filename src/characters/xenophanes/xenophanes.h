c769dca4eba1ea071c2ed6e8cb4631ce9e11bf1e
g3yt <83846251+g3yt@users.noreply.github.com>
2023-02-01 14:32:26 +0000

    fixed
    i am so cool

--- src/characters/xenophanes/xenophanes.h
+++ src/characters/xenophanes/xenophanes.h
@@ -1,9 +1,9 @@
-#ifndef PSXF_GUARD_LORDX_H
-#define PSXF_GUARD_LORDX_H
+#ifndef PSXF_GUARD_XENO_H
+#define PSXF_GUARD_XENO_H
 
 #include "../../stage/character.h"
 
 //Xenophanes character functions
-Character *Char_Dad_New(fixed_t x, fixed_t y);
+Character *Char_Xeno_New(fixed_t x, fixed_t y);
 
 #endif
